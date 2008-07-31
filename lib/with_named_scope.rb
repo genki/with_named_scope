fa63827850f2ec6eebde35bb189df2fb1f2d567b
Genki Takiuchi <genki@s21g.com>
2008-08-01 05:45:58 +0900

    Fixed a bug.

--- lib/with_named_scope.rb
+++ lib/with_named_scope.rb
@@ -1,3 +1,29 @@
+require 'active_record'
+require 'active_record/named_scope'
+
 class WithNamedScope
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
+end
+
+module ActiveRecord
+  class Base
+    class << self
+    protected
+      def with(&block)
+        block.call if block
+      end
+    end
+  end
+
+  module NamedScope
+    class Scope
+      def with(&block)
+        create = proxy_options[:conditions].is_a?(Hash) ?
+          proxy_options[:conditions] : {}
+        with_scope :find => proxy_options, :create => create do
+          proxy_scope.send :with, &block
+        end
+      end
+    end
+  end
 end
